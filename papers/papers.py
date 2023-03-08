59e60b4e52bd984c39404e7050bead6c38a29c3f
S1M0N38 <bertolottosimone@gmail.com>
2023-03-08 10:21:12 +0100

    Genereation of selected papers (by refs)

--- papers/papers.py
+++ papers/papers.py
@@ -3,14 +3,16 @@ import pathlib
 import yaml
 
 here = pathlib.Path(__file__).parent
+
+# Useful papers
+
 md = "> This file was generated from papis library using papers.py script\n\n"
 md += (
-    "# Papers \n"
     "This is a collection of papers from *ICCV2021*, *ICCV2019*, *CVPR2022*, "
     "*CVPR2021*, *CVPR2020* and (TODO *CVPR2019 ?*, *NeurIPS2022*, "
-    "*NeurIPS2021*, *NeurIPS2020*)\n"
+    "*NeurIPS2021*, *NeurIPS2020*) "
     "regarding adversarial attack/defence.\n"
-    "\n---\n"
+    "\n---------------------------------------------------------------------\n"
 )
 
 for paper in here.glob("**/info.yaml"):
@@ -22,14 +24,45 @@ for paper in here.glob("**/info.yaml"):
         if code := info.get("code"):
             md += f" [[code]({code})]"
         md += "\n\n"
-        try:
-            md += f"{info['notes']}\n"
-        except KeyError as e:
-            print(info["title"])
-            raise e
-
+        md += f"{info['abstract']}\n"
         md += "\n---\n"
 
-
 with open(here / "papers.md", "w") as f:
+    f.write(md)
+
+
+# Selected papers
+
+selected = {
+    "1909.09481v1",
+    "1912.09405v4",
+    "2108.07969v1",
+    "2104.12669v3",
+    "2010.04925v4",
+    "2011.11164v2",
+    "1904.00887v4",
+    "1903.09799v3",
+}
+
+md = "> This file was generated from papis library using papers.py script\n\n"
+md += (
+    "This is a collection of papers selected from papers.md.\n"
+    "\n---------------------------------------------------------------------\n"
+)
+
+for paper in here.glob("**/info.yaml"):
+    with open(paper, "r") as f:
+        info: dict = yaml.safe_load(f)
+        if info["ref"] not in selected:
+            continue
+
+        md += f"- **{info['title']}**, {info['year']}"
+        md += f" [[url]({info['url']})]"
+        if code := info.get("code"):
+            md += f" [[code]({code})]"
+        md += f"\n>{info['author']}\n"
+        md += f"\n{info['abstract']}\n"
+        md += "\n---\n"
+
+with open(here / "selected.md", "w") as f:
     f.write(md)
