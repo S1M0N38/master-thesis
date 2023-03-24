0d0d88bfbd27c04c0b686f3f00dd3ba327175f29
S1M0N38 <bertolottosimone@gmail.com>
2023-03-24 10:40:26 +0100

    Add map from fine to coarse label for CIFAR-100

--- code/data.py
+++ code/data.py
@@ -121,6 +121,17 @@ classes_CIFAR100 = (
     ("lawn_mower", "rocket", "streetcar", "tank", "tractor"),
 )
 
+# fmt: off
+fine_to_coarse_CIFAR100 = [
+     4,  1, 14,  8,  0,  6,  7,  7, 18,  3,  3, 14,  9, 18,  7, 11,  3,  9,  7, 11,
+     6, 11,  5, 10,  7,  6, 13, 15,  3, 15,  0, 11,  1, 10, 12, 14, 16,  9, 11,  5,
+     5, 19,  8,  8, 15, 13, 14, 17, 18, 10, 16,  4, 17,  4,  2,  0, 17,  4, 18, 17,
+    10,  3,  2, 12, 12, 16, 12,  1,  9, 19,  2, 10,  0,  1, 16, 12,  9, 13, 15, 13,
+    16, 19,  2,  4,  6, 19,  5,  5,  8, 19, 18,  1,  2, 15,  6,  0, 17,  8, 14, 13,
+]
+# fmt: on
+
+# Keep using Che+19a normalization
 transform_train_CIFAR100 = transforms.Compose(
     [
         transforms.RandomCrop(32, padding=4),
@@ -165,3 +176,6 @@ testloader_CIFAR100 = DataLoader(
     worker_init_fn=seed_worker,
     generator=g,
 )
+
+if __name__ == "__main__":
+    breakpoint()
